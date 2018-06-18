5d4d70da1bcf3edb1a3e0dc2fa5139e57abf8429
Marouen Helali <marwanest@live.fr>
2018-06-18 10:53:49 -0500

    few edits

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-hero-detail',
@@ -9,7 +10,11 @@ export class HeroDetailComponent implements OnInit {
 
   constructor() { }
 
+
+
   ngOnInit() {
   }
 
 }
+
+@input() hero: Hero;
